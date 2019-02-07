cb6b3a4b595f03f988b85eac2ffc946b31217e96
pierodecassan <decassan@gmail.com>
2019-02-07 11:40:33 +0100

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
-
+const port = process.env.PORT || 3000;
 var app = express();
 
 // app.use((req, res, next) => {
@@ -70,4 +70,6 @@ hbs.registerHelper('screamIt', (testo) => {
    );
  })
 
-app.listen(3000);
+app.listen(port, () =>{
+console.log(`Server is up on port ${port}`);
+});
